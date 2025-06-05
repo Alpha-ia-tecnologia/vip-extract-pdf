974a15149af0993feb67bf9bd82e0c37ff231d04
Arthur <78182643+ArthurSousa460@users.noreply.github.com>
2025-06-05 19:56:47 -0300

    Update app.py
    add cors

--- app/app.py
+++ app/app.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
 from app.controllers.extractDataCarController import router_car
 from app.controllers.extractDataHouseController import router_house
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(router_car)
 app.include_router(router_house)
+
+
 
 @app.get("/api")
 async def read_root():
